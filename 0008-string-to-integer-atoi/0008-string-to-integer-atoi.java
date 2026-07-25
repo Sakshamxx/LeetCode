73a9e5b1df322272ab75e03a6e156c2e0b52782c
Saksham Chauhan <sakshamchauhan003@gmail.com>
2026-07-26 00:38:10 +0530

    Time: 1 ms (100%), Space: 43.8 MB (41.74%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int myAtoi(String s) {
+        // Whitespace
+        s = s.trim();
+        // Base case
+        if(s.isEmpty()){
+            return 0;
+        }
+        // Signedness
+        int i=0;
+        int sign=1;
+        if (s.charAt(i)== '-' || s.charAt(i)== '+'){
+            sign =s.charAt(i) == '-' ? -1:1;
+            i++;
+        }
+        // Conversion
+        long num = 0;
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
+            num = (num*10) + (s.charAt(i)-'0');
+            if (num*sign>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }else if (num*sign<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+        return (int)(sign*num);
+    }
+}
